713b2b0047819b56c818ab7ec67fe96ba37e5a2d
Makuochukwu225 <officialrrye5@gmail.com>
2022-12-27 00:58:01 +0100

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,3 +52,45 @@ window.addEventListener("scroll", function () {
     backTopBtn.classList.remove("active");
   }
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var openBtn = document.getElementById('open-btn');
+  var closeBtn = document.getElementById('close-btn');
+  var dialog = document.getElementById('dialog');
+
+  openBtn.addEventListener('click', function() {
+    dialog.style.display = 'block';
+  });
+
+  closeBtn.addEventListener('click', function() {
+    dialog.style.display = 'none';
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var openPrivacyPolicyBtn = document.getElementById('open-privacy-policy-btn');
+  var closePrivacyPolicyBtn = document.getElementById('close-privacy-policy-btn');
+  var privacyPolicyDialog = document.getElementById('privacy-policy-dialog');
+  openPrivacyPolicyBtn.addEventListener('click', function() {
+    privacyPolicyDialog.style.display = 'block';
+  });
+  
+  closePrivacyPolicyBtn.addEventListener('click', function() {
+    privacyPolicyDialog.style.display = 'none';
+  });
+})  
+
+document.addEventListener('DOMContentLoaded', function() {
+  var openPrivacyPolicyBtn = document.getElementById('open-terms-and-condition-btn');
+  var closePrivacyPolicyBtn = document.getElementById('close-terms-and-condition-btn');
+  var privacyPolicyDialog = document.getElementById('terms-and-condition-dialog');
+  openPrivacyPolicyBtn.addEventListener('click', function() {
+    privacyPolicyDialog.style.display = 'block';
+  });
+  
+  closePrivacyPolicyBtn.addEventListener('click', function() {
+    privacyPolicyDialog.style.display = 'none';
+  });
+})  
